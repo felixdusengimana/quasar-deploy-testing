6ff3d8d5efa441db29a3f3c7d0914955d369ea46
Nasir Lawal <nasirlawal001@gmail.com>
2022-08-09 20:01:09 +0100

    style bar chart(active/offline) colors

--- src/pages/dashboard/DashboardPage.vue
+++ src/pages/dashboard/DashboardPage.vue
@@ -16,39 +16,47 @@
 import { defineComponent } from 'vue';
 import ChartSection from 'src/components/dashboard/ChartSection.vue';
 
-const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      display: true,
-      position: 'top',
-      align: 'end',
-      labels: {
-        usePointStyle: true,
-        boxWidth: 10,
-      },
-    },
-  },
-}
 const chartData = {
   labels: ['week1', 'week2', 'week3', 'week4', 'week1', 'week2', 'week3', 'week4', 'week1', 'week2', 'week3', 'week4', 'week1', 'week2', 'week3', 'week4'],
   datasets: [
     {
       label: 'Offline',
+      backgroundColor: '#1e136b',
       data: [4, 5, 7, 6, 4, 5, 7, 6, 4, 5, 7, 6, 4, 5, 7, 6, 4, 5, 7, 6, 4, 5, 7, 6, 4, 5, 7, 6, 4, 5, 7, 6],
-      fill: false,
-      tension: 0.4,
-      borderColor: '#FF6132',
+      borderWidth: 1,
     },
     {
-      label: 'Active',
-      data: [1, 2, 3, 2, 4, 5, 7, 3, 4, 6, 3, 6, 3, 6, 7, 3, 6, 8, 3, 2, 3, 5, 7, 2, 1 ,3, 1, 3, 4, 6, 4, 4],
-      fill: false,
-      tension: 0.4,
-      borderColor: '#80B2FE',
+      label: 'Offline',
+      backgroundColor: '#faa5e8',
+      data: [5, 3, 2, 1, 3, 2, 4, 2, 3, 1, 4, 2, 7, 3, 2, 5, 2, 4, 1, 4, 1, 2, 3, 7, 2, 7, 2, 1, 6, 7, 1, 4],
+      borderWidth: 1,
     },
-  ],
-}
+  ]
+};
+
+const options = {
+  type: 'bar',
+  data: chartData,
+  scales: {
+    y: {
+      ticks: {
+        stepSize: 0
+      }
+    }
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'First semester attendance'
+      }
+    }
+  },
+};
 export default defineComponent({
   name: "DashboardPage",
   components: {
