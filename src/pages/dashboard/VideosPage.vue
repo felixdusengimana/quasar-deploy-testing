20dc4298d3f55175f0fba1cd748aab5250b341a7
Nasir Lawal <nasirlawal001@gmail.com>
2022-08-10 13:31:59 +0100

    add next and previous button section

--- src/pages/dashboard/VideosPage.vue
+++ src/pages/dashboard/VideosPage.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <div class="tw-border-2 tw-py-6 tw-mx-4 tw-mt-5 tw-rounded-lg tw-px-10">
+      <div class="tw-text-base">
+        UI/UX Design
+      </div>
       <q-linear-progress
         :value="progress"
         rounded
@@ -25,12 +28,25 @@
         </div>
       </div>
     </div>
-    <div class="tw-w-2/3 tw-mt-12 tw-ml-6">
-      <q-media-player
-        type="video"
-        :sources="sources"
-      />
-    </div>
+    <div class="tw-flex">
+      <div class="tw-w-2/3 tw-mt-12 tw-ml-6">
+        <q-media-player
+          type="video"
+          :sources="sources"
+        />
+      </div>
+      <div class="tw-mt-14 tw-w-1/3 tw-ml-10">
+        <div class="tw-flex tw-justify-center">
+          <q-btn unelevated color="accent" label="Previous" no-caps class="tw-h-10 tw-w-24 tw-rounded-full" @click="onClick" />
+          <div class="tw-w-24 tw-h-10 tw-mx-4 tw-border-2 tw-border-primary-bg-color tw-rounded-full">
+            <q-btn flat color="accent" label="Next" no-caps class="tw-h-10 tw-w-24 tw-rounded-full" @click="onClick" />
+          </div>
+        </div>
+        <div class="tw-text-base tw-pt-5">
+          First semester - month 1
+        </div>
+      </div>
+    </div>    
   </div>
 </template>
 
@@ -43,7 +59,6 @@ export default defineComponent({
     const sources = [
       {
         src: 'http://www.peach.themazzone.com/durian/movies/sintel-2048-surround.mp4',
-        type: 'video/mp4'
       }
     ]
     const progress = ref(0.3)
