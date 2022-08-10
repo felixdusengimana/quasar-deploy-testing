9e16e5104f23dd32333dcd8c3a38075d2c5fbeb9
Nasir Lawal <nasirlawal001@gmail.com>
2022-08-10 12:33:22 +0100

    add progress bar

--- src/pages/dashboard/VideosPage.vue
+++ src/pages/dashboard/VideosPage.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
+    <div class="tw-border-2 tw-py-6 tw-mx-4 tw-mt-5 tw-rounded-lg tw-px-10">
+      <q-linear-progress
+        :value="progress"
+        rounded
+        size="25px"
+        color="accent"
+        track-color="grey3"
+        class="q-mt-sm"
+      >
+        <div class="absolute-full flex flex-center">
+          <q-badge color="white" text-color="accent" :label="progressLabel" />
+        </div>
+      </q-linear-progress>
+      <div class="tw-flex tw-justify-between tw-mt-2">
+        <div>
+          1st semester
+        </div>
+        <div>
+          2nd semester
+        </div>
+        <div>
+          3rd semester
+        </div>
+      </div>
+    </div>
     <div class="tw-w-2/3 tw-mt-12 tw-ml-6">
       <q-media-player
         type="video"
@@ -10,15 +35,10 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-import { QMediaPlayer } from '@quasar/quasar-ui-qmediaplayer';
-import '@quasar/quasar-ui-qmediaplayer/src/index.sass';
+import { computed, defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: "VideosPage",
-  // components: {
-  //   QMediaPlayer
-  // },
   setup() {
     const sources = [
       {
@@ -26,9 +46,12 @@ export default defineComponent({
         type: 'video/mp4'
       }
     ]
+    const progress = ref(0.3)
 
     return {
-      sources
+      sources,
+      progress,
+      progressLabel: computed(() => (progress.value * 100).toFixed(2) + '%'),
     }
   },
 })
