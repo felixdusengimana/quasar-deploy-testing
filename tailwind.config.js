f3fb3859116aa692145daefbbdc8c3143bd0350a
phelixdusengimana <phelixdusengimana@gmail.com>
2022-09-21 12:39:07 +0200

    feat: added shadow and default font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,13 @@ module.exports = {
       },
       backgroundImage: {
         'auth-bg-image': "url('src/assets/img/auth-bg.png')"
-      }
+      },
+      boxShadow: {
+        'form': '0px 0px 1.67244px rgba(0, 0, 0, 0.12), 0px 16.7244px 16.7244px rgba(0, 0, 0, 0.08)',
+      },
+      fontFamily: {
+        'manrope': ['Manrope', 'sans-serif']
+      },
     },
   },
   plugins: [],
