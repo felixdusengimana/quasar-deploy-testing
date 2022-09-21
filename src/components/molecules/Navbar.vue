bf791a46af4f6df90faab318ef97400acdc11d8d
phelixdusengimana <phelixdusengimana@gmail.com>
2022-09-21 12:40:18 +0200

    feat: register, login and register success page done✅

---
+++ src/components/molecules/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div :class="isWhite? 'tw-bg-white tw-border-b tw-border-[#000060]':'tw-bg-primary-dark-color'" class="tw-h-20 tw-w-full tw-flex tw-justify-center tw-items-center">
+        <img v-if="isWhite" src="../../assets/img/logo/black-logo.svg" alt="Educacity Logo">
+        <img v-else src="../../assets/img/logo/white-logo.svg" alt="Educacity Logo">
+    </div>
+</template>
+
+<script lang="js">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'navbar-component',
+    props: {
+        isWhite: Boolean
+    },
+    setup(props) {
+        props.isWhite
+    }
+});
+</script>
+
