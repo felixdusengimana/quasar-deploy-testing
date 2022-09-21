f41b60f13ca07b63fa6b1f63c962e03f70cc0cf3
phelixdusengimana <phelixdusengimana@gmail.com>
2022-09-21 19:26:15 +0200

    feat: responsive on screens

---
+++ src/components/molecules/RegisterForm.vue
@@ -0,0 +1,168 @@
+<template>
+      <div class="tw-flex tw-items-center tw-flex-col tw-w-full">
+        <q-form
+        @submit="onSubmit"
+        @reset="onReset"
+        class="tw-mt-10 tw-w-fit tw-text-center"
+      >
+        <div class="tw-text-center">
+          <h1 class="tw-text-black tw-text-2xl tw-font-bold">Personal Information</h1>
+          <p class="tw-text-[#000000] tw-mt-2 tw-mb-5 tw-text-xl">Fill in your details</p>
+          </div>
+        
+    <div class="tw-flex tw-flex-row tw-mt-20 tw-justify-between tw-w-full tw-gap-8">
+        <input
+          type="text"
+          v-model="firstName"
+          name="firstName"
+          placeholder="First name"
+          class="lg:tw-w-[472px] tw-py-6 tw-p-4 tw-border tw-border-[#000000] tw-mb-6 tw-text-sm tw-outline-none"
+        />
+        <input
+          type="text"
+          v-model="lastName"
+          name="lastName"
+          placeholder="Last name"
+          class="lg:tw-w-[472px] tw-py-6 tw-p-4 tw-border tw-border-[#000000] tw-mb-6 tw-text-sm tw-outline-none"
+        />
+    </div>
+         
+    <div class="tw-flex tw-flex-row tw-justify-between tw-w-full tw-gap-8">
+        <input
+          type="phone"
+          v-model="phone"
+          name="phone"
+          placeholder="Phone Number (+2349034848075)"
+          class="lg:tw-w-[472px] tw-py-6 tw-p-4 tw-border tw-border-[#000000] tw-mb-6 tw-text-sm tw-outline-none"
+        />
+        <select
+          v-model="gender"
+          name="gender"
+          placeholder="Gender"
+          class="lg:tw-w-[472px] tw-py-6 tw-p-4 tw-border tw-border-[#000000] tw-mb-6 tw-text-sm tw-outline-none"
+        >
+        <option value="" selected="selected" disabled>Select gender</option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+        <option value="others">Others</option>
+      </select>
+    </div>
+
+    <div class="tw-flex tw-flex-row tw-justify-between tw-w-full tw-gap-8">
+        <select
+          v-model="country"
+          name="country"
+          class="lg:tw-w-[472px] tw-py-6 tw-p-4 tw-border tw-border-[#000000] tw-mb-6 tw-text-sm tw-outline-none"
+        >
+      <option value="" disabled selected>Country of Residence</option>
+        <option value="USA">USA</option>
+        <option value="UK">UK</option>
+        <option value="Nigeria">Nigeria</option>
+        <option value="Rwanda">Rwanda</option>
+    </select>
+        <select
+          v-model="state"
+          name="state"
+          class="lg:tw-w-[472px] tw-py-6 tw-p-4 tw-border tw-border-[#000000] tw-mb-6 tw-text-sm tw-outline-none"
+        >
+     <option value="" disabled selected>State of Residence</option>
+        <option value="USA">USA</option>
+        <option value="UK">UK</option>
+        <option value="Nigeria">Nigeria</option>
+        <option value="Rwanda">Rwanda</option>
+    </select>
+    </div>
+
+    <div class="tw-flex tw-flex-row tw-justify-between tw-w-full tw-gap-8">
+        <input
+          type="text"
+          v-model="educationLevel"
+          name="educationLevel"
+          placeholder="Level of education"
+          class="lg:tw-w-[472px] tw-py-6 tw-p-4 tw-border tw-border-[#000000] tw-mb-6 tw-text-sm tw-outline-none"
+        />
+        <input
+          type="date"
+          v-model="dob"
+          name="dob"
+          class="lg:tw-w-[472px] tw-py-6 tw-p-4 tw-border tw-border-[#000000] tw-mb-6 tw-text-sm tw-outline-none"
+        />
+    </div>
+
+
+    <div class="tw-flex tw-flex-row tw-justify-between tw-w-full tw-gap-8">
+        <select
+          v-model="school"
+          name="school"
+          class="lg:tw-w-[472px] tw-py-6 tw-p-4 tw-border tw-border-[#000000] tw-mb-6 tw-text-sm tw-outline-none"
+        >
+       <option value="" selected disabled>School  of Choice</option>
+        <option value="MIT">MIT</option>
+        <option value="Stanford">Stanford</option>
+        <option value="Harvard">Harvard</option>
+        <option value="Oxford">Oxford</option>
+    </select>
+        <select
+          v-model="course"
+          name="course"
+          class="lg:tw-w-[472px] tw-py-6 tw-p-4 tw-border tw-border-[#000000] tw-mb-6 tw-text-sm tw-outline-none"
+        >
+      <option value="" selected disabled>Course</option>
+        <option value="Computer Science">Computer Science</option>
+        <option value="Engineering">Engineering</option>
+        <option value="Medicine">Medicine</option>
+        <option value="Law">Law</option>
+    </select>
+    </div>
+
+    <div class="lg:tw-w-[474px] tw-h-[148px] tw-border tw-border-[#000000] tw-border-dashed tw-rounded-[10px] tw-mx-auto tw-mb-16">
+        <label for="uploadFile" class="tw-flex tw-flex-col tw-items-center tw-justify-center tw-h-full">
+            <img src="../../assets/icons/file-upload.svg" alt=""/>
+            <p class="tw-text-[#000000] tw-text-base tw-font-bold">Click here to upload your SSCE certificate</p>
+         <input type="file" hidden name="file" accept=".pdf" id="uploadFile">
+        </label>
+        
+    </div>
+          <button type="submit" class="tw-max-w-[640px] tw-bg-[#000060] tw-h-[67px] tw-w-full tw-text-white tw-rounded-none tw-text-sm tw-capitalize">
+            Continue
+          </button>
+      </q-form>
+      <div class="tw-mt-5 tw-mb-10">
+        <p class="tw-text-[#8692A6] tw-text-base tw-flex tw-items-center">
+          <img src="../../assets/icons/lock.svg" alt="" class="tw-mr-2" />
+          <span>Your Info is safely secured</span></p>
+      </div>
+      </div>
+</template>
+
+<script>
+   import { ref } from 'vue'
+  export default {
+    name: 'register-form',
+    props: {
+      nextStep: {type: Function, required: false}
+    },
+    setup(props) {
+      const email = ref('')
+      const password = ref('')
+  
+      const onSubmit = (evt) => {
+        evt.preventDefault()
+        console.log('submit', email.value, password.value)
+        props.nextStep()
+      }
+  
+      const onReset = (evt) => {
+        evt.preventDefault()
+        console.log('reset')
+      }
+  
+      return {
+        email,
+        password,
+        onSubmit,
+        onReset
+      }
+    }
+  }
+</script>
--- src/pages/auth/LoginScreen.vue
+++ src/pages/auth/LoginScreen.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="tw-h-screen tw-font-manrope">
+  <div class="tw-min-h-screen tw-font-manrope tw-flex tw-flex-col">
     <Navbar isWhite/>
-    <div class="tw-flex tw-justify-around tw-h-[90%] tw-items-center tw-flex-col tw-w-full">
+    <div class="tw-w-fit tw-mx-auto">
       <q-form
       @submit="onSubmit"
       @reset="onReset"
-      class="tw-w-[536.02px] tw-mt-10 tw-border-[0.84px] tw-border-black tw-rounded-lg tw-p-8 tw-shadow-form"
+      class="lg:tw-w-[55vh]  tw-mt-10 tw-border-[0.84px] tw-border-black tw-rounded-lg tw-p-8 tw-shadow-form"
     >
-      <div class="tw-flex tw-h-10 tw-items-center tw-justify-end">
-        <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M15.9443 5.41406L5.90967 15.4487" stroke="#2C3E50" stroke-width="1.67244" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M5.90967 5.41406L15.9443 15.4487" stroke="#2C3E50" stroke-width="1.67244" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-      </div>
       <div class="tw-text-center">
         <h1 class="tw-text-black tw-text-xl tw-font-bold">Log in to Eduvacity</h1>
         <p class="tw-text-[#606060] tw-mt-2 tw-mb-5">Don't have account? <router-link to="/register" class="tw-text-[#4A7DFF]">Signup</router-link></p>
@@ -22,7 +16,7 @@
         v-model="email"
         name="email"
         placeholder="Enter your email"
-        class="tw-w-full tw-py-6 tw-p-4 tw-border tw-border-[#000000] tw-mb-6 tw-text-sm tw-outline-none"
+        class="md:tw-w-full lg:tw-w-full tw-w-11/12 tw-py-6 tw-p-4 tw-border tw-border-[#000000] tw-mb-6 tw-text-sm tw-outline-none"
       />
        
         <input
@@ -30,27 +24,26 @@
         v-model="password"
         name="password"
         placeholder="Enter your password"
-        class="tw-w-full tw-py-6 tw-p-4 tw-border tw-border-[#000000] tw-mb-6 tw-text-sm tw-outline-none"/>
+        class="md:tw-w-full lg:tw-w-full tw-w-11/12 tw-py-6 tw-p-4 tw-border tw-border-[#000000] tw-mb-6 tw-text-sm tw-outline-none"/>
       <div>
         <label for="remember" class="tw-flex tw-flex-row tw-gap-1 tw-text-sm tw-mb-2">
           <input class="tw-h-[14.22px]" type="checkbox" name="remember" id="remember"/> 
           <p>Rember me</p>
         </label>
-        <button type="submit" class="tw tw-bg-[#000060] tw-h-[67px] tw-w-full tw-text-white tw-rounded-none tw-text-sm tw-capitalize">
+        <button type="submit" class="md:tw-w-full lg:tw-w-full tw-w-11/12 tw-bg-[#000060] tw-h-[67px] tw-text-white tw-rounded-none tw-text-sm tw-capitalize">
           Continue
         </button>
         <a href="" class="tw-text-[#1E136B] tw-text-sm tw-block tw-my-5 tw-text-center tw-underline tw-cursor-pointer">Forgot password?</a>
 
-        <p class="m tw-max-w-[267.56px] tw-text-[#8F969D] tw-mx-auto tw-text-center tw-text-sm">
+        <p class="tw-max-w-[267.56px]  tw-text-[#8F969D] tw-mx-auto tw-text-center tw-text-sm">
           by continuing you  agree to eduvacity <a href="" class="tw-text-[#4A7DFF]">Terms & Condition</a> and <a href="" class="tw-text-[#4A7DFF]">Privacy Policy</a>
         </p>
       </div>
     </q-form>
-    <div>
-      <p class="tw-text-[#8692A6] tw-text-base tw-flex tw-items-center">
+      <p class="tw-text-[#8692A6] tw-text-base tw-flex tw-items-center tw-my-5 tw-w-fit tw-mx-auto">
         <img src="../../assets/icons/lock.svg" alt="" class="tw-mr-2" />
-        <span>Your Info is safely secured</span></p>
-    </div>
+        <span>Your Info is safely secured</span>
+      </p>
     </div>
   </div>
 </template>
