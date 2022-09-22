d3917783d9e1df89c1d7b1c15730e66e650fdcb7
phelixdusengimana <phelixdusengimana@gmail.com>
2022-09-22 18:49:37 +0200

    feat: setting up dashboard environment

---
+++ src/components/dashboard/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navbar-component",
+  props: {
+    user: {
+      type: Object,
+      default: () => {}
+    },
+    setting: {
+      type: Object,
+      default: () => {}
+    },
+  },
+  data() {
+    return {
+      drawer: null,
+    };
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer;
+    },
+  },
+};
+</script>
---
+++ src/components/dashboard/Sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "sidebar-component",
+    props: {
+      user: {
+        type: Object,
+        default: () => {}
+      },
+      setting: {
+        type: Object,
+        default: () => {}
+      },
+    },
+  }
+</script>
