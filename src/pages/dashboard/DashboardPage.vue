229a7b58f8d778f1ec5da798667c316274d166a9
phelixdusengimana <phelixdusengimana@gmail.com>
2022-09-25 19:39:42 +0200

    feat: half way to finishing dashboard home

--- src/pages/dashboard/DashboardPage.vue
+++ src/pages/dashboard/DashboardPage.vue
@@ -1,62 +1,35 @@
 <template>
-  <div>
-    <div class="tw-px-4 tw-pt-5 tw-text-2xl text-accent tw-font-medium">
-      Welcome John Doe!
-    </div>
-    <div class="tw-border-2 tw-py-6 tw-mx-4 tw-mt-5 tw-rounded-lg tw-px-10">
-      <q-linear-progress
-        :value="progress"
-        rounded
-        size="25px"
-        color="accent"
-        track-color="grey3"
-        class="q-mt-sm"
-      >
-        <div class="absolute-full flex flex-center">
-          <q-badge color="white" text-color="accent" :label="progressLabel" />
-        </div>
-      </q-linear-progress>
-      <div class="tw-flex tw-justify-between tw-mt-2">
-        <div>
-          1st semester
-        </div>
-        <div>
-          2nd semester
-        </div>
-        <div>
-          3rd semester
-        </div>
+  <div class="tw-mt-5 tw-max-w-[1121.73px] tw-border tw-border-[#D9D9D9] tw-flex tw-mx-auto tw-rounded-xl tw-px-10">
+    <div class="tw-py-8 tw-border-r-[2px] tw-pr-10">
+      <h1 class="tw-text-black tw-font-semibold tw-text-3xl tw-mb-2">Welcome back Musa!</h1>
+      <div class="tw-flex tw-gap-2 tw-items-center tw-mb-3">
+        <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 21V18.6667L2.33333 16.3333V21H0ZM4.66667 21V14L7 11.6667V21H4.66667ZM9.33333 21V11.6667L11.6667 14.0292V21H9.33333ZM14 21V14.0292L16.3333 11.6958V21H14ZM18.6667 21V9.33333L21 7V21H18.6667ZM0 14.9625V11.6667L8.16667 3.5L12.8333 8.16667L21 0V3.29583L12.8333 11.4625L8.16667 6.79583L0 14.9625Z" fill="#C62828"/>
+        </svg>
+        <p class="tw-text-black tw-font-normal tw-text-base">You are behind your weekly goals</p>
       </div>
-    </div>
-    <q-card flat class="tw-mx-auto">
-      <q-card-section>
-        <div class="tw-border-2 tw-rounded-lg tw-p-10">
-          <div class="tw-text-gray-600 tw-text-base">
-            First semester attendance
+       <div class="tw-flex tw-items-center tw-gap-3 tw-mb-16">
+        <p class="tw-font-bold tw-text-sm">Timline 17 Aug - 30 Aug</p>
+        <q-btn class="tw-bg-[#1E136B] tw-capitalize tw-text-white tw-text-xs tw-max-w-[52px] tw-h-[23px]">Edit</q-btn>
+       </div>
+
+       <div class="tw-flex tw-gap-3 tw-items-center">
+        <img class="tw-object-cover tw-max-w-[184px] tw-h-[129px] tw-rounded-lg" src="~assets/img/course_default.png" alt="">
+        <div>
+          <p class="tw-text-black tw-font-bold tw-text-base">Intoduction to Product design</p>
+          <div class="tw-flex justify-between tw-items-center tw-mb-3 tw-mt-2">
+            <p class="tw-text-black tw-font-normal tw-text-sm">Week 1</p>
+            <p class="tw-h-[49px] tw-w-[1.5px] tw-bg-[#D9D9D9]"></p>
+            <p class="tw-text-black tw-font-normal tw-text-sm">7 min</p>
+            <q-btn class="tw-bg-[#FB676530] tw-border tw-capitalize tw-border-[#FB6765] tw-rounded-[4px] tw-text-black tw-font-normal tw-text-sm tw-py-[6px]">Resume</q-btn>
           </div>
-          <div class="tw-flex tw-justify-between tw-mt-2">
-            <div class="tw-text-left tw-pl-5">
-              first month
-            </div>
-            <div class="tw-text-left tw-ml-3">
-              second month
-            </div>
-            <div class="tw-text-left">
-              third month
-            </div>
-            <div>
-            </div>
-          </div>
-          <ChartSection
-            ref="chart"
-            chartId="comparisons-chart"
-            type="bar"
-            :data="chartData"
-            :options="options"
-          />
+          <q-linear-progress></q-linear-progress>
+          <p class="tw-mt-2 tw-text-black tw-text-sm tw-font-normal">Lesson progress</p>
         </div>
-      </q-card-section>
-    </q-card>
+
+       </div>
+    </div>
+    <div></div>
   </div>
 </template>
 
@@ -107,7 +80,7 @@ const options = {
 export default defineComponent({
   name: "DashboardPage",
   components: {
-    ChartSection
+    // ChartSection
   },
   setup() {
     const progress = ref(0.33)
