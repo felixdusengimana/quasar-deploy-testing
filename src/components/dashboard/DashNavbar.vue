08879d8c5b0098e3890301f1a6fcc62650aa8be5
phelixdusengimana <phelixdusengimana@gmail.com>
2022-10-11 18:41:15 +0200

    feat: added  profile dropdown in header

--- src/components/dashboard/DashNavbar.vue
+++ src/components/dashboard/DashNavbar.vue
@@ -23,10 +23,27 @@
             </div>
           </template>
             <q-list>
-              <q-item clickable v-close-popup @click="onItemClick">
+              <q-item>
                 <q-item-section>
-                  <q-item-label>Photos</q-item-label>
+                  <q-item-label class="tw-w-full tw-text-[#9F9F9F] tw-font-bold">My profile</q-item-label>
                 </q-item-section>
+              </q-item>
+              <q-item>
+                <q-item-section>
+                  <div class="tw-flex tw-gap-4 tw-items-center">
+                    <div class="tw-w-9 tw-h-9 tw-bg-[#FFDC5D] tw-rounded-full"></div>
+                    <div>
+                      <p class="tw-font-bold tw-text-sm">Aminu Abdulrasheed</p>
+                      <p class="tw-leading-[19px] tw-text-sm tw-text-[#606060]">Product Manager - Admin</p>
+                    </div>
+                  </div>
+                </q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup @click="onItemClick">
+                <div class="tw-flex tw-items-center tw-gap-4 tw-cursor-pointer">
+                      <img src="~assets/icons/logout.svg"/>
+                      <p class="tw-text-[#606060] tw-font-semibold tw-text-sm tw-leading-[19px]">Log out</p>
+                    </div>
               </q-item>
             </q-list>
           </q-btn-dropdown>
