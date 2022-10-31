13a52427f2cf8761f75d19013e22a5e13ae6917e
phelixdusengimana <phelixdusengimana@gmail.com>
2022-10-31 18:59:51 +0200

    feat: f

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hello there</h1>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
