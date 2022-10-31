4ed523cebc4ffa223ccd80d3724c45424dd902f5
phelixdusengimana <phelixdusengimana@gmail.com>
2022-10-31 21:32:09 +0200

    fix: hashtag

--- quasar.config.js
+++ quasar.config.js
@@ -61,7 +61,7 @@ module.exports = configure(function (ctx) {
         node: 'node16'
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       distDir: ctx.mode.spa ? 'public' : null,
       // vueRouterBase,
       // vueDevtools,
