b67eb090597cac587f34e2e7620f443b496d9c5f
phelixdusengimana <phelixdusengimana@gmail.com>
2022-11-03 12:16:34 +0200

    feat: PDF viewer

--- src/pages/dashboard/ProfilePage.vue
+++ src/pages/dashboard/ProfilePage.vue
@@ -2,13 +2,13 @@
   <div class="tw-absolute tw-w-full">
     <DashPageTitle :title="'Aminu Abdulrasheed - Product design'"/>
   </div>
-  <div class="tw-container tw-flex tw-items-center tw-h-full tw-font-manrope tw-gap-6">
+  <div class="tw-container tw-flex tw-items-center tw-h-full tw-font-manrope tw-gap-6 tw-mt-16">
    <div class="tw-flex tw-flex-col tw-items-center tw-w-3/12 tw-h-auto">
     <ProfileLeftSide :fullname="'Aminu Abdulrasheed'" :title="'Product design student'" :type="isApplicant?'application':'profile'"/>
    </div>
    <div class="tw-border-l-2 tw-border-[#DFDFE0] tw-h-full"></div>
    <div class="tw-grow">
-     <ProfileRightSide :country="'Nigeria'" :course="'Design'" :dateofbirth="'05/20/2022'" :state="'Kaduna'" :gender="'male'"/>
+     <ProfileRightSide :country="'Nigeria'" :course="'Design'" :dateofbirth="'05/20/2022'" :state="'Kaduna'" :gender="'male'" :relatedDocuments="[1,2]"/>
     </div>
   </div>
 </template>
